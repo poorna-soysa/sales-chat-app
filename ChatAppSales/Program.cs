79c2bfa74b961ef78c3af5c232e7845685ec6246
Poorna Soysa <poornapiumal.soysa@gmail.com>
2025-09-15 00:07:26 +0530

    Cleaned Program.cs

--- ChatAppSales/Program.cs
+++ ChatAppSales/Program.cs
@@ -11,10 +11,6 @@ using ChatAppSales.Entities;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-// You will need to set the endpoint and key to your own values
-// You can do this using Visual Studio's "Manage User Secrets" UI, or on the command line:
-//   cd this-project-directory
-//   dotnet user-secrets set GitHubModels:Token YOUR-GITHUB-TOKEN
 var credential = new ApiKeyCredential(builder.Configuration["GitHubModels:Token"] ?? throw new InvalidOperationException("Missing configuration: GitHubModels:Token. See the README for details."));
 var openAIOptions = new OpenAIClientOptions()
 {
@@ -26,15 +22,10 @@ var chatClient = ghModelsClient.GetChatClient("gpt-4o-mini").AsIChatClient();
 var embeddingGenerator = ghModelsClient.GetEmbeddingClient("text-embedding-3-small").AsIEmbeddingGenerator();
 
 var vectorStorePath = Path.Combine(AppContext.BaseDirectory, "vector-store.db");
-//var vectorStoreConnectionString = $"Data Source={vectorStorePath}";
-//builder.Services.AddSqliteCollection<string, IngestedChunk>("data-chatappsales-chunks", vectorStoreConnectionString);
-//builder.Services.AddSqliteCollection<string, IngestedDocument>("data-chatappsales-documents", vectorStoreConnectionString);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//builder.Services.AddScoped<DataIngestor>();
-//builder.Services.AddSingleton<SemanticSearch>();
 builder.Services.AddScoped<IMetricsService, MetricsService>();
 
 builder.Services.AddChatClient(chatClient).UseFunctionInvocation().UseLogging();
@@ -61,14 +52,6 @@ app.MapGet("/api/Seed", async (AppDbContext appDb) =>
 {
     await DemoSeeder.SeedAsync(appDb, 3);
 });
-
-// By default, we ingest PDF files from the /wwwroot/Data directory. You can ingest from
-// other sources by implementing IIngestionSource.
-// Important: ensure that any content you ingest is trusted, as it may be reflected back
-// to users or could be a source of prompt injection risk.
-//await DataIngestor.IngestDataAsync(
-//    app.Services,
-//    new PDFDirectorySource(Path.Combine(builder.Environment.WebRootPath, "Data")));
 
 
 app.Run();
